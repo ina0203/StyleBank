07ece2457dd0e79f5d98db60ef4065187413b216
ina0203 <78330097+ina0203@users.noreply.github.com>
2026-01-09 10:40:47 +0900

    Remove App component definition
    Removed the App component definition from main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>StyleBank</h1>
-      <p>Vite + React + GitHub Pages</p>
-    </div>
-  );
-}
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
